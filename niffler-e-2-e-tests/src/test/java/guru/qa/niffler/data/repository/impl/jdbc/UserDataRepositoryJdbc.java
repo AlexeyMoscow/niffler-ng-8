4c8e152fc4b9bd002cfa618bcb197a858498a0f3
Alexey <alex.nowik@mail.ru>
2025-04-27 18:36:30 +0300

    HomeWork 6.2 - Repository for springJdbc

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/repository/impl/jdbc/UserDataRepositoryJdbc.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/repository/impl/jdbc/UserDataRepositoryJdbc.java
@@ -17,129 +17,129 @@ import static guru.qa.niffler.data.entity.userdata.FriendshipStatus.PENDING;
 import static guru.qa.niffler.data.tpl.Connections.holder;
 
 public class UserDataRepositoryJdbc implements UserDataRepository {
-  private static final Config CFG = Config.getInstance();
+    private static final Config CFG = Config.getInstance();
 
-  @Override
-  public UserEntity createUser(UserEntity user) {
-    try (PreparedStatement ps = holder(CFG.userdataJdbcUrl()).connection().prepareStatement(
-            "INSERT INTO \"user\" (username, currency, firstname, surname, full_name, photo, photo_small) " +
-                    "VALUES (?, ?, ?, ?, ?, ?, ? )",
-            Statement.RETURN_GENERATED_KEYS
-    )) {
-      ps.setString(1, user.getUsername());
-      ps.setString(2, user.getCurrency().name());
-      ps.setString(3, user.getFirstname());
-      ps.setString(4, user.getSurname());
-      ps.setString(5, user.getFullname());
-      ps.setBytes(6, user.getPhoto());
-      ps.setBytes(7, user.getPhotoSmall());
-      ps.executeUpdate();
-      final UUID generatedKey;
-      try (ResultSet rs = ps.getGeneratedKeys()) {
-        if (rs.next()) {
-          generatedKey = rs.getObject("id", UUID.class);
-        } else {
-          throw new SQLException("Cant find id in ResultSet");
+    @Override
+    public UserEntity createUser(UserEntity user) {
+        try (PreparedStatement ps = holder(CFG.userdataJdbcUrl()).connection().prepareStatement(
+                "INSERT INTO \"user\" (username, currency, firstname, surname, full_name, photo, photo_small) " +
+                        "VALUES (?, ?, ?, ?, ?, ?, ? )",
+                Statement.RETURN_GENERATED_KEYS
+        )) {
+            ps.setString(1, user.getUsername());
+            ps.setString(2, user.getCurrency().name());
+            ps.setString(3, user.getFirstname());
+            ps.setString(4, user.getSurname());
+            ps.setString(5, user.getFullname());
+            ps.setBytes(6, user.getPhoto());
+            ps.setBytes(7, user.getPhotoSmall());
+            ps.executeUpdate();
+            final UUID generatedKey;
+            try (ResultSet rs = ps.getGeneratedKeys()) {
+                if (rs.next()) {
+                    generatedKey = rs.getObject("id", UUID.class);
+                } else {
+                    throw new SQLException("Cant find id in ResultSet");
+                }
+            }
+            user.setId(generatedKey);
+            return user;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
-      }
-      user.setId(generatedKey);
-      return user;
-    } catch (SQLException e) {
-      throw new RuntimeException(e);
     }
-  }
 
-  @Override
-  public Optional<UserEntity> findById(UUID id) {
-    try (PreparedStatement ps = holder(CFG.userdataJdbcUrl()).connection().prepareStatement(
-            "SELECT * FROM \"user\" WHERE id = ?"
-    )) {
-      ps.setObject(1, id);
-      ps.execute();
-      try (ResultSet rs = ps.getResultSet()) {
-        if (rs.next()) {
-          UserEntity ue = new UserEntity();
+    @Override
+    public Optional<UserEntity> findById(UUID id) {
+        try (PreparedStatement ps = holder(CFG.userdataJdbcUrl()).connection().prepareStatement(
+                "SELECT * FROM \"user\" WHERE id = ?"
+        )) {
+            ps.setObject(1, id);
+            ps.execute();
+            try (ResultSet rs = ps.getResultSet()) {
+                if (rs.next()) {
+                    UserEntity ue = new UserEntity();
 
-          ue.setId(rs.getObject("id", UUID.class));
-          ue.setUsername(rs.getString("username"));
-          ue.setCurrency(CurrencyValues.valueOf(rs.getString("currency")));
-          ue.setFirstname((rs.getString("firstname")));
-          ue.setSurname((rs.getString("surname")));
-          ue.setFullname((rs.getString("full_name")));
-          ue.setPhoto((rs.getBytes("photo_small")));
-          ue.setPhotoSmall((rs.getBytes("photoSmall")));
-          return Optional.ofNullable(ue);
-        } else {
-          return Optional.empty();
+                    ue.setId(rs.getObject("id", UUID.class));
+                    ue.setUsername(rs.getString("username"));
+                    ue.setCurrency(CurrencyValues.valueOf(rs.getString("currency")));
+                    ue.setFirstname((rs.getString("firstname")));
+                    ue.setSurname((rs.getString("surname")));
+                    ue.setFullname((rs.getString("full_name")));
+                    ue.setPhoto((rs.getBytes("photo_small")));
+                    ue.setPhotoSmall((rs.getBytes("photoSmall")));
+                    return Optional.ofNullable(ue);
+                } else {
+                    return Optional.empty();
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
-      }
-    } catch (SQLException e) {
-      throw new RuntimeException(e);
     }
-  }
 
-  @Override
-  public Optional<UserEntity> findByUsername(String username) {
-    try (PreparedStatement ps = holder(CFG.userdataJdbcUrl()).connection().prepareStatement(
-            "SELECT * FROM \"user\" WHERE username = ?"
-    )) {
-      ps.setObject(1, username);
-      ps.execute();
-      try (ResultSet rs = ps.getResultSet()) {
-        if (rs.next()) {
-          UserEntity ue = new UserEntity();
+    @Override
+    public Optional<UserEntity> findByUsername(String username) {
+        try (PreparedStatement ps = holder(CFG.userdataJdbcUrl()).connection().prepareStatement(
+                "SELECT * FROM \"user\" WHERE username = ?"
+        )) {
+            ps.setObject(1, username);
+            ps.execute();
+            try (ResultSet rs = ps.getResultSet()) {
+                if (rs.next()) {
+                    UserEntity ue = new UserEntity();
 
-          ue.setId(rs.getObject("id", UUID.class));
-          ue.setUsername(rs.getString("username"));
-          ue.setCurrency(CurrencyValues.valueOf(rs.getString("currency")));
-          ue.setFirstname((rs.getString("firstname")));
-          ue.setSurname((rs.getString("surname")));
-          ue.setFullname((rs.getString("full_name")));
-          ue.setPhoto((rs.getBytes("photo_small")));
-          ue.setPhotoSmall((rs.getBytes("photoSmall")));
-          return Optional.ofNullable(ue);
-        } else {
-          return Optional.empty();
+                    ue.setId(rs.getObject("id", UUID.class));
+                    ue.setUsername(rs.getString("username"));
+                    ue.setCurrency(CurrencyValues.valueOf(rs.getString("currency")));
+                    ue.setFirstname((rs.getString("firstname")));
+                    ue.setSurname((rs.getString("surname")));
+                    ue.setFullname((rs.getString("full_name")));
+                    ue.setPhoto((rs.getBytes("photo_small")));
+                    ue.setPhotoSmall((rs.getBytes("photoSmall")));
+                    return Optional.ofNullable(ue);
+                } else {
+                    return Optional.empty();
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
-      }
-    } catch (SQLException e) {
-      throw new RuntimeException(e);
     }
-  }
 
-  @Override
-  public void addFriend(UserEntity requester, UserEntity addressee) {
-    try (PreparedStatement ps = holder(CFG.userdataJdbcUrl()).connection().prepareStatement(
-            "INSERT INTO friendship (requester_id, addressee_id, status) " +
-                    "VALUES (?, ?, ? )"
-    )) {
-      ps.setObject(1, requester.getId());
-      ps.setObject(2, addressee.getId());
-      ps.setString(3, ACCEPTED.name());
-      ps.executeUpdate();
+    @Override
+    public void addFriend(UserEntity requester, UserEntity addressee) {
+        try (PreparedStatement ps = holder(CFG.userdataJdbcUrl()).connection().prepareStatement(
+                "INSERT INTO friendship (requester_id, addressee_id, status) " +
+                        "VALUES (?, ?, ? )"
+        )) {
+            ps.setObject(1, requester.getId());
+            ps.setObject(2, addressee.getId());
+            ps.setString(3, ACCEPTED.name());
+            ps.executeUpdate();
 
-      ps.setObject(1, addressee.getId());
-      ps.setObject(2, requester.getId());
-      ps.setString(3, ACCEPTED.name());
-      ps.executeUpdate();
-    } catch (SQLException e) {
-      throw new RuntimeException(e);
+            ps.setObject(1, addressee.getId());
+            ps.setObject(2, requester.getId());
+            ps.setString(3, ACCEPTED.name());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
-  }
 
-  @Override
-  public void addFriendshipRequest(UserEntity requester, UserEntity addressee) {
-    try (PreparedStatement ps = holder(CFG.userdataJdbcUrl()).connection().prepareStatement(
-            "INSERT INTO friendship (requester_id, addressee_id, status) " +
-                    "VALUES (?, ?, ? )"
-    )) {
-      ps.setObject(1, requester.getId());
-      ps.setObject(2, addressee.getId());
-      ps.setString(3, PENDING.name());
-      ps.executeUpdate();
-    } catch (SQLException e) {
-      throw new RuntimeException(e);
+    @Override
+    public void addFriendshipRequest(UserEntity requester, UserEntity addressee) {
+        try (PreparedStatement ps = holder(CFG.userdataJdbcUrl()).connection().prepareStatement(
+                "INSERT INTO friendship (requester_id, addressee_id, status) " +
+                        "VALUES (?, ?, ? )"
+        )) {
+            ps.setObject(1, requester.getId());
+            ps.setObject(2, addressee.getId());
+            ps.setString(3, PENDING.name());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
-  }
 
 }
--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/UsersDbClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/UsersDbClient.java
@@ -8,7 +8,11 @@ import guru.qa.niffler.data.entity.auth.Authority;
 import guru.qa.niffler.data.entity.auth.AuthorityEntity;
 import guru.qa.niffler.data.entity.userdata.UserEntity;
 import guru.qa.niffler.data.repository.AuthUserRepository;
+import guru.qa.niffler.data.repository.UserDataRepository;
 import guru.qa.niffler.data.repository.impl.jdbc.AuthUserRepositoryJdbc;
+import guru.qa.niffler.data.repository.impl.jdbc.UserDataRepositoryJdbc;
+import guru.qa.niffler.data.repository.impl.spring.AuthUserRepositorySpringJdbc;
+import guru.qa.niffler.data.repository.impl.spring.UserDataRepositorySpringJdbc;
 import guru.qa.niffler.data.tpl.DataSources;
 import guru.qa.niffler.data.tpl.XaTransactionTemplate;
 import guru.qa.niffler.model.UserJson;
@@ -26,6 +30,10 @@ public class UsersDbClient {
   private static final PasswordEncoder pe = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 
   private final AuthUserRepository authUserRepository = new AuthUserRepositoryJdbc();
+  private final UserDataRepository userDataRepository = new UserDataRepositoryJdbc();
+
+  private final AuthUserRepository authUserSpringRepository = new AuthUserRepositorySpringJdbc();
+  private final UserDataRepository userDataSpringRepository = new UserDataRepositorySpringJdbc();
   private final UdUserDao udUserDao = new UdUserDaoSpringJdbc();
 
   private final TransactionTemplate txTemplate = new TransactionTemplate(
@@ -66,4 +74,43 @@ public class UsersDbClient {
         }
     );
   }
+
+    public UserJson xaCreateUserSpringRepository(UserJson user) {
+        return xaTransactionTemplate.execute(() -> {
+            AuthUserEntity authUserEntity = new AuthUserEntity();
+            authUserEntity.setUsername(user.username());
+            authUserEntity.setPassword(pe.encode("12345"));
+            authUserEntity.setEnabled(true);
+            authUserEntity.setAccountNonExpired(true);
+            authUserEntity.setAccountNonLocked(true);
+            authUserEntity.setCredentialsNonExpired(true);
+            authUserEntity.setAuthorities(
+                    Arrays.stream(Authority.values()).map(
+                            e -> {
+                                AuthorityEntity ae = new AuthorityEntity();
+                                ae.setUser(authUserEntity);
+                                ae.setAuthority(e);
+                                return ae;
+                            }).toList()
+            );
+
+            authUserSpringRepository.create(authUserEntity);
+
+            return UserJson.fromEntity(
+                    userDataSpringRepository.createUser(UserEntity.fromJson(user)
+                    )
+                    ,null);
+        });
+    }
+
+    public void addFriend(UserJson user, UserJson friend) {
+        xaTransactionTemplate.execute(() -> {
+            userDataRepository.addFriend(UserEntity.fromJson(user), UserEntity.fromJson(friend));
+            return null;
+        });
+    }
+
+    public void addInvitation(UserJson requester, UserJson addressee){
+        userDataRepository.addFriendshipRequest(UserEntity.fromJson(requester), UserEntity.fromJson(addressee));
+    }
 }
